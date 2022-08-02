743573269cb190608c5f47be090e0f60ecb73590
amoriah <yonoyko@bk.ru>
2022-08-02 20:26:08 +0300

    start ex04

--- 00/ex01/src/main.cpp
+++ 00/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yono <yono@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 11:44:10 by amoriah           #+#    #+#             */
-/*   Updated: 2022/08/02 16:57:59 by yono             ###   ########.fr       */
+/*   Updated: 2022/08/02 18:15:04 by yono             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	printHead()
 	std::cout << " " << std::endl;
 	std::cout << " |--------------------------PHONEBOOK---------------------------|" << std::endl;
 	std::cout << " " << std::endl;
-	std::cout << "What would you like: ADD, SEARCH or EXIT? Go ahead!" << std::endl;	
+	std::cout << "Hello^^ What would you like: ADD, SEARCH or EXIT? Go ahead!" << std::endl;	
 }
 
 int	main()
@@ -40,7 +40,7 @@ int	main()
 			break;
 		else
 			std::cout << "Oops, PhoneBook is so dummy, doesn't know such command, try again. " << std::endl;		
-		std::cout << "Write a command: ADD, SEARCH or EXIT? Go ahead!" << std::endl;
+		std::cout << "Write a command: ADD, SEARCH or EXIT?" << std::endl;
 	} while (cmd != "EXIT" || cmd != "exit");
 	std::cout << "Bye, my Lil pumpkin!" << std::endl;
 		return 0;
--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -1,11 +1,66 @@
-/*Создайте программу, которая принимает три параметра 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yono <yono@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 20:07:03 by yono              #+#    #+#             */
+/*   Updated: 2022/08/02 20:24:24 by yono             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cerrno>
+#include <cstdlib>
+
+static void printErr(std::string err)
+{
+	std::cerr << err << std::endl;
+	exit(1);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+		printErr("Error: bad arguments");
+	std::string *filename = av[1].c_str();
+	std::string s1;
+	std::string s2;
+	std::string buf;
+	std::ifstream fin(av[1]);
+	std::ofstream fout;
+
+	if (!fin.is_open())
+		printErr("Error: cannot open file");
+	if (!!getline(fin, buf))
+		printErr("Error: invalid file");
+	fout.open(filename.append(".replace"));
+	if (!fin.is_open())
+		printErr("CAN NOT CREATE FILENAME.replace");
+	while (!fin.eof() && !fin.fail())
+	{
+		if (buf == s1)
+			fout << s2 << std::endl;
+		else
+			fout << buf << std::endl;
+		getline(fin, buf);
+	}
+	fin.close();
+	fout.close();
+	return (0);
+}
+
+/*Создайте программу, которая принимает три параметра
 в следующем порядке: имя файла и две строки, s1 и s2.
-Она откроет файл <filename> и скопирует его содержимое 
+Она откроет файл <filename> и скопирует его содержимое
 в новый файл <filename>.replace, заменяя каждое вхождение s1 на s2.
-Использование функций манипулирования файлами на 
-языке C запрещено и будет считаться мошенничеством. 
+Использование функций манипулирования файлами на
+языке C запрещено и будет считаться мошенничеством.
 Все функции-члены класса std::string разрешены, кроме replace. Используйте их с умом!
-Конечно, обрабатывайте неожиданные входы и ошибки. 
-Вы должны создавать и сдавать свои собственные тесты, 
+Конечно, обрабатывайте неожиданные входы и ошибки.
+Вы должны создавать и сдавать свои собственные тесты,
 чтобы убедиться, что ваша программа работает так, как ожидается.
  */
